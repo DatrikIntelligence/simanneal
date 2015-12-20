5dd71d1106d17d718ff3aec392defa6da9308a6a
David <dasolma@gmail.com>
2015-12-20 21:55:25 +0100

    missing self ref

--- simanneal/anneal.py
+++ simanneal/anneal.py
@@ -122,14 +122,14 @@ class Annealer(object):
 
         elapsed = time.time() - self.start
         if step == 0:
-            if verbose:
+            if self.verbose:
                 print(' Temperature        Energy    Accept   Improve     Elapsed   Remaining')
                 sys.stdout.write('\r%12.2f  %12.2f                      %s            ' % \
                     (T, E, time_string(elapsed)))
                 sys.stdout.flush()
         else:
             remain = (self.steps - step) * (elapsed / step)
-            if verbose:
+            if self.verbose:
                 sys.stdout.write('\r%12.2f  %12.2f  %7.2f%%  %7.2f%%  %s  %s' % \
                 (T, E, 100.0 * acceptance, 100.0 * improvement,\
                 time_string(elapsed), time_string(remain))),
